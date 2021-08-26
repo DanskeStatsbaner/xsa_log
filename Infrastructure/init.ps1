47ec82fd16034dc096176c779644cc56cd2faed8
XLAMI1707 <xlmil@dsb.dk>
2021-08-26 10:49:33 +0200

    Update init.ps1 and remove other ps1 files

--- Infrastructure/init.ps1
+++ Infrastructure/init.ps1
@@ -1,4 +1,4 @@
-﻿$environment = $OctopusParameters["Octopus.Environment.Name"]
+$environment = $OctopusParameters["Octopus.Environment.Name"]
 
 # exit if environment is Sandbox (sit)
 if($environment -eq "sit") { Exit }
@@ -17,22 +17,35 @@ $artifactoryPW = $args[0]
 $login = $OctopusParameters["artifactory.login"]
 $registry = $OctopusParameters["artifactory.registry"]
 $projectName = $OctopusParameters["Octopus.Project.Name"]
-$releaseNumber = $OctopusParameters["Octopus.Release.Number"]
-$containerName = "dataArt.$($projectName).$($releaseNumber).$($environment)"
+#$releaseNumber = $OctopusParameters["Octopus.Release.Number"]
+#$containerName = "dataArt.$($projectName).$($releaseNumber).$($environment)"
+$containerName = "dataArt.$($projectName).$($environment)"
 
 ###############################################################################
 # Stop and delete containers
 ###############################################################################
 
+write-host "docker prune: $containerName"
+
 if ($(docker container ls -aq -f name="$containerName").length -gt 0){ docker container stop $($containerName) }
 docker container prune -f
+
+write-host "docker prune complete: $containerName" 
 
 ###############################################################################
 # Login to artifactory, pull and start XSA_CLI_DEPLOY container
 ###############################################################################
 
+write-host "login: -u $login -p $artifactoryPW   $registry"
+
 docker login -u $login -p $artifactoryPW   $registry
+
+Write-host "login complete - now pull"
+
 docker pull artifactory.azure.dsb.dk/docker/xsa_cli_deploy
+
+Write-host "pull complete - now run"
+
 docker run -v C:\Octopus\Work:/data --name $containerName --rm -t -d artifactory.azure.dsb.dk/docker/xsa_cli_deploy
 
 write-host "*******************************************************************"
